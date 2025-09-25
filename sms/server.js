c886226c9012470dada2b0d746ea705fd8cbd4e4
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-09-26 00:24:45 +0530

    changed few things in sms

--- sms/server.js
+++ sms/server.js
@@ -6,19 +6,18 @@ app.use(bodyParser.json());
 
 app.post("/api/sms/incoming", (req, res) => {
     const { from, message } = req.body;
-
+    console.log("Received SMS data:", req.body);
     if (!from || !message) {
-        return res.status(400).json({ success: false, error: "Missing fields" });
+        console.log("❌ Missing fields:", req.body);
+        return res.status(200).json({ success: false, error: "Missing fields" });
     }
 
     console.log("📩 SMS Received:");
     console.log("From:", from);
     console.log("Message:", message);
 
-    res.json({ success: true, error: null });
+    res.status(200).json({ success: true, error: null });
 });
 
-
-
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
