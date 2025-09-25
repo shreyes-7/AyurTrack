c1044335def21891140b54e5f6a533ccabcf1b1c
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-09-26 02:17:58 +0530

    errpr

--- sms/server.js
+++ sms/server.js
@@ -1,22 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-    console.error("✅ Root route hit");
+    console.log("✅ Root route hit");
     res.send("Server is live");
 });
 
+
 app.post("/api/sms/incoming", (req, res) => {
     const { from, message } = req.body;
-    console.error("Received SMS data:", req.body);
+    console.log("Received SMS data:", req.body);
     if (!from || !message) {
-        console.error("❌ Missing fields:", req.body);
+        console.log("❌ Missing fields:", req.body);
         return res.status(200).json({ success: false, error: "Missing fields" });
     }
 
-    console.error("📩 SMS Received:");
-    console.error("From:", from);
-    console.error("Message:", message);
+
+    console.log("📩 SMS Received:");
+    console.log("From:", from);
+    console.log("Message:", message);
 
     res.status(200).json({ success: true, error: null });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.error(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
