aa46ec069a73bb834584c4835999b5684598cb8c
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-09-25 20:49:10 +0530

    resolved the qualtiTest route file fetching error

--- backend/src/routes/v1/consumer.route.js
+++ backend/src/routes/v1/consumer.route.js
@@ -1,14 +1,12 @@
 // src/routes/consumer.route.js
 const express = require('express');
 const validate = require('../../middlewares/validate');
-const consumerValidation = require('../../validations/consumer.validation');
 const { consumerController } = require('../../controllers');
 
 const router = express.Router();
 
 // Single API endpoint for complete product information
 router.get('/product/:batchId',
-    validate(consumerValidation.getBatchTraceability),
     consumerController.getCompleteProductInfo
 );
 
--- backend/src/routes/v1/qualityTest.route.js
+++ backend/src/routes/v1/qualityTest.route.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const validate = require('../../middlewares/validate');
 const auth = require('../../middlewares/auth');
-const qualityTestValidation = require('../../validations/qualityTest.validation');
+const {qualityTestValidation} = require('../../validations')
 const { qualityTestController } = require('../../controllers');
 
 const router = express.Router();
